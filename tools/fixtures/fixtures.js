4fd45ea88cfe747eec3e1e9a0858c9f8be47cf57
Baptiste Leulliette <bleulliette@hotmail.fr>
2018-01-22 20:04:08 +0100

    create offer in fixtures + show offers on map (LAGGY as FCK)

--- tools/fixtures/fixtures.js
+++ tools/fixtures/fixtures.js
@@ -2,13 +2,18 @@ const Kuzzle = require('kuzzle-sdk')
 const parse = require('csv-parse')
 const bluebird = require('bluebird')
 const fs = require('fs')
+const randomWords = require('random-words')
+const uuidv4 = require('uuid/v4')
+const randomFloat = require('random-float')
+const randomInt = require('random-int')
 
 Kuzzle.prototype.bluebird = bluebird
-let kuzzle = new Kuzzle('localhost')
+let kuzzle = new Kuzzle('127.0.0.1')
 
 let output = []
 
 let data = []
+let offers = []
 
 function createFixtures () {
   try {
@@ -110,6 +115,99 @@ function createFixtures () {
   }
 }
 
+function createOffersFixtures (callback) {
+  offers = []
+  var count = 1
+
+  for (var i = 0; i < 1000; i++) {
+    /**
+     Ref GPS POINT
+    'lat': 43.62613531177415,
+    'lon': 3.9056396484375
+    */
+    var document = {
+      'jobCode': randomInt(10200, 10500),
+      'jobPosition': {
+        'lat': '',
+        'lon': ''
+      },
+      'title': randomWords({min: 3, max: 10, join: ' '}),
+      'description': randomWords({min: 30, max: 80, join: ' '}),
+      'societyName': randomWords(),
+      'societyAddress': randomWords({min: 3, max: 10, join: ' '}),
+      'societyWebsite': '',
+      'experience': [],
+      'salary': getRandomInt(120000)
+    }
+
+    var lat
+    var lon
+    if (count === 1) {
+      lat = 43.62613531177415 + randomFloat(1)
+      lon = 3.9056396484375 + randomFloat(1)
+      count++
+    } else if (count === 2) {
+      lat = 43.62613531177415 - randomFloat(1)
+      lon = 3.9056396484375 - randomFloat(1)
+      count++
+    } else if (count === 3) {
+      lat = 43.62613531177415 - randomFloat(1)
+      lon = 3.9056396484375 + randomFloat(1)
+      count++
+    } else if (count === 4) {
+      lat = 43.62613531177415 + randomFloat(1)
+      lon = 3.9056396484375 - randomFloat(1)
+      count++
+    } else {
+      count = 1
+    }
+
+    document.jobPosition.lat = lat
+    document.jobPosition.lon = lon
+
+    var experience = getRandomInt(3)
+    if (experience === 0 || experience === 1) {
+      experience = {
+        'key': 1,
+        'label': 'Débutant'
+      }
+    } else if (experience === 2) {
+      experience = {
+        'key': 2,
+        'label': 'Confirmé'
+      }
+    } else if (experience === 3) {
+      experience = {
+        'key': 3,
+        'label': 'Senior'
+      }
+    }
+    document.experience.push(experience)
+
+    var offerType = getRandomInt(7)
+    if (offerType === 0) {
+      offerType = 1
+    }
+    document.offerType = offerType
+
+    offers.push({
+      create: {
+        _index: 'offers',
+        _type: 'data',
+        _id: uuidv4()
+      }
+    })
+    offers.push(document)
+  }
+
+  console.log('Done generating offers')
+  callback()
+}
+
+function getRandomInt (max) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
 function insertData () {
   kuzzle.loginPromise('local', {username: 'admin', password: 'admin'}, '1h')
     .then(() => kuzzle.queryPromise({index: 'labels'}, {controller: 'index', action: 'delete'}))
@@ -123,8 +221,9 @@ function insertData () {
     .then(() => kuzzle.collection('subCategories', 'labels').createPromise())
     .then(() => kuzzle.collection('jobs', 'labels').createPromise())
     .then(() => kuzzle.queryPromise({controller: 'bulk', action: 'import'}, {body: {bulkData: data}}))
+    .then(() => kuzzle.queryPromise({controller: 'bulk', action: 'import'}, {body: {bulkData: offers}}))
     .then(response => {
-      console.log('done !')
+      // console.log('done !')
       process.exit(0)
     }).catch(error => {
       console.error(error)
@@ -132,4 +231,6 @@ function insertData () {
     })
 }
 
-createFixtures()
+createOffersFixtures(() => {
+  createFixtures()
+})
